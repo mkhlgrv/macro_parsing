45d90b164b85decfd4a0e011a989c53d258630a7
Mikhail Gareev <grvmkhl@gmail.com>
2021-06-01 23:56:53 +0300

    Update run.R
    Add run algorithm for fred

--- R/run.R
+++ R/run.R
@@ -1,4 +1,11 @@
+library(dplyr)
+# library(data.table)
+# library(future)
+# library(purrr)
+# plan(multisession)
 # MOEX
+
+check.raw.files()
 new('moex') %>%
   ticker('IMOEX') %>%
   observation.start %>%
@@ -6,3 +13,19 @@ new('moex') %>%
   date.from %>%
   download.ts %>%
   write.ts
+
+new('fred') %>%
+  ticker('SP500') %>%
+  freq %>%
+  observation.start %>%
+  previous.date.till %>%
+  date.from %>%
+  download.ts%>%
+  write.ts
+
+
+new('oecd') %>%
+
+
+
+

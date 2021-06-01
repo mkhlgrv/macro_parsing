a0a666216e21708107c5e684aff4048a55e27b1a
Mikhail Gareev <grvmkhl@gmail.com>
2021-06-01 22:45:21 +0300

    Create fred_parsing.R
    Methods for parsing from FRED

---
+++ R/fred_parsing.R
@@ -0,0 +1,85 @@
+
+setClass('fred',
+         slots = list(freq = 'factor'),
+         contains = 'parsed_ts')
+
+setMethod("initialize", "parsed_ts",
+          function(.Object,
+                   ticker,
+                   observation_start,
+                   date_from,
+                   ts
+          ) {             
+            .Object@ticker <- character()
+            .Object@observation_start <- lubridate::ymd()
+            .Object@previous_date_till <- lubridate::ymd()
+            .Object@date_from <- lubridate::ymd()
+            .Object@ts <- tibble::tibble(date = lubridate::ymd(),
+                                         value = numeric(), 
+                                         update_date = lubridate::ymd())
+            .Object@freq <- factor(levels = c('d', 'w', 'm'))
+            validObject(.Object)
+            return(.Object)
+          }
+)
+
+setMethod("freq", "fred",
+          function(object
+          ) {
+            object@freq <- data.table::fread('data/info/var_list.csv',
+                                                          select = c('ticker', 'freq')) %>%
+              .[which(.$ticker==object@ticker),] %>%
+              .$freq %>%
+              factor(levels = c('d', 'w', 'm'))
+            validObject(object)
+            return(object)
+          }
+)
+
+setMethod("date.from", "fred",
+          function(object
+          ) {
+            
+            
+            if(length(object@previous_date_till)==0){
+              object@date_from <- object@observation_start %>%
+                lubridate::ymd()
+            } else {
+              date_from <- object@previous_date_till %>%
+                lubridate::ymd()
+              if(object@freq == 'm'){
+                object@date_from <- zoo::as.yearmon(date_from+31) %>%
+                  zoo::as.Date() %>%
+                  lubridate::ymd()
+              } else{
+                object@date_from <- lubridate::ymd(date_from+1)
+              }
+            }
+
+            validObject(object)
+            return(object)
+          }
+)
+
+
+setMethod("download.ts", "fred",
+          function(object
+          ) {
+            fredr::fredr_set_key(Sys.getenv('fredr_api_key'))
+            
+            object@ts <- fredr::fredr(object@ticker,
+            frequency = object@freq %>% as.character,
+            observation_start = object@date_from) %>%
+              dplyr::select(date, value) %>%
+              dplyr::mutate(date = as.Date(date),
+                     update_date = as.Date(Sys.Date())) %>%
+              dplyr::arrange(date, update_date)
+            
+            validObject(object)
+            return(object)
+          }
+)
+
+
+
+

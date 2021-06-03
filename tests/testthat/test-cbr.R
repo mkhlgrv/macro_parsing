657e626d9035a9866e3a71a37e239235ca080988
Mikhail Gareev <grvmkhl@gmail.com>
2021-06-03 21:22:04 +0300

    Create test-cbr.R
    Simple test

---
+++ tests/testthat/test-cbr.R
@@ -0,0 +1,20 @@
+setwd('C:/Users/Mikhail Gareev/Documents/macroparsing')
+test_that("Test cbr methods", {
+  cbr_ts <- new('cbr') %>%
+    ticker('fer') %>%
+    freq %>%
+    observation.start %>%
+    previous.date.till %>%
+    date.from %>%
+    cbr.ticker %>%
+    url %>%
+    download.ts()
+  expect_equal(cbr_ts@url,
+               'https://www.cbr.ru/eng/hd_base/mrrf/mrrf_7d/?UniDbQuery.Posted=True&UniDbQuery.From=05/1998&UniDbQuery.To=06/2021')
+  expect_s3_class(cbr_ts, 'cbr')
+  expect_equal(cbr_ts@ticker,"fer")
+  expect_equal(cbr_ts@cbr_ticker,"mrrf/mrrf_7d")
+  expect_equal(cbr_ts@observation_start,'1998-05-29')
+  expect_equal(ncol(cbr_ts@ts),3)
+
+})

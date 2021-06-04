081aff8bf9d43f977f873f6f9e3ae575b97adc2c
Mikhail Gareev <grvmkhl@gmail.com>
2021-06-04 11:14:40 +0300

    Save info files as data

---
+++ R/dataset_doc.R
@@ -0,0 +1,70 @@
+#' Here is the title of the dataset
+#'
+#' @description More description of the dataset is here
+#'
+#' @docType data
+#'
+#' @usage data("var_list")
+#'
+#' @format data.table
+#'
+#' @keywords datasets
+#'
+#' @source This is a link to
+#' @export
+#' \href{http://www.xxx.xxx.com/xxx.csv}{the origin data}
+#'
+"variables"
+
+#' Here is the title of the dataset
+#'
+#' @description More description of the dataset is here
+#'
+#' @docType data
+#'
+#' @usage data("var_list")
+#'
+#' @format data.table
+#'
+#' @keywords datasets
+#'
+#' @source This is a link to
+#' \href{http://www.xxx.xxx.com/xxx.csv}{the origin data}
+#'
+"sources"
+
+#' Here is the title of the dataset
+#'
+#' @description More description of the dataset is here
+#'
+#' @docType data
+#'
+#' @usage data("var_list")
+#'
+#' @format data.table
+#'
+#' @keywords datasets
+#'
+#' @source This is a link to
+#' \href{http://www.xxx.xxx.com/xxx.csv}{the origin data}
+#'
+
+"cbr_names"
+
+#' Here is the title of the dataset
+#'
+#' @description More description of the dataset is here
+#'
+#' @docType data
+#'
+#' @usage data("var_list")
+#'
+#' @format data.table
+#'
+#' @keywords datasets
+#'
+#' @source This is a link to
+#' \href{http://www.xxx.xxx.com/xxx.csv}{the origin data}
+#'
+
+"oecd_names"
--- R/run.R
+++ R/run.R
@@ -51,6 +51,17 @@
 #   download.ts %>%
 #   write.ts()
 #
+
+
+# save info data ----
+
+# variables <- data.table::fread('inst/extdata/info/var_list.csv', encoding = 'UTF-8')
+# cbr_names <- data.table::fread('inst/extdata/info/cbr_name_list.csv', encoding = 'UTF-8')
+# sources <- data.table::fread('inst/extdata/info/source_list.csv', encoding = 'UTF-8')
+# oecd_names <- data.table::fread('inst/extdata/info/oecd_name_list.csv', encoding = 'UTF-8')
 #
-#
-#
+# usethis::use_data(variables, variables)
+# usethis::use_data(cbr_names, cbr_names)
+# usethis::use_data(sources, sources)
+# usethis::use_data(oecd_names, oecd_names)
+

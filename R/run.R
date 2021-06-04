2c887faaf5bd1c0bfd8798e1125e2976ea04763c
Mikhail Gareev <grvmkhl@gmail.com>
2021-06-04 07:38:36 +0300

    Update run.R
    Comment all lines

--- R/run.R
+++ R/run.R
@@ -1,56 +1,56 @@
-library(dplyr)
-# library(data.table)
-# library(future)
-# library(purrr)
-# plan(multisession)
-# MOEX
-
-check.raw.files()
-new('moex') %>%
-  ticker('IMOEX') %>%
-  observation.start %>%
-  previous.date.till %>%
-  date.from %>%
-  download.ts %>%
-  write.ts
-
-new('fred') %>%
-  ticker('SP500') %>%
-  freq %>%
-  observation.start %>%
-  previous.date.till %>%
-  date.from %>%
-  download.ts%>%
-  write.ts
-
-
-new('oecd') %>%
-  ticker('cli_RUS') %>%
-  observation.start %>%
-  oecd.ticker %>%
-  url %>%
-  download.ts%>%
-  write.ts
-
-new('igrea') %>%
-  ticker('igrea') %>%
-  observation.start %>%
-  url %>%
-  download.ts%>%
-  write.ts
-
-  
-new('cbr') %>%
-  ticker('export_usd') %>%
-  freq %>%
-  observation.start %>%
-  previous.date.till %>%
-  date.from %>% 
-  cbr.ticker %>%
-  url %>%
-  download.ts %>%
-  write.ts()
-
-
-
-
+# library(dplyr)
+# # library(data.table)
+# # library(future)
+# # library(purrr)
+# # plan(multisession)
+# # MOEX
+#
+# check.raw.files()
+# new('moex') %>%
+#   ticker('IMOEX') %>%
+#   observation.start %>%
+#   previous.date.till %>%
+#   date.from %>%
+#   download.ts %>%
+#   write.ts
+#
+# new('fred') %>%
+#   ticker('SP500') %>%
+#   freq %>%
+#   observation.start %>%
+#   previous.date.till %>%
+#   date.from %>%
+#   download.ts%>%
+#   write.ts
+#
+#
+# new('oecd') %>%
+#   ticker('cli_RUS') %>%
+#   observation.start %>%
+#   oecd.ticker %>%
+#   url %>%
+#   download.ts%>%
+#   write.ts
+#
+# new('igrea') %>%
+#   ticker('igrea') %>%
+#   observation.start %>%
+#   url %>%
+#   download.ts%>%
+#   write.ts
+#
+#
+# new('cbr') %>%
+#   ticker('export_usd') %>%
+#   freq %>%
+#   observation.start %>%
+#   previous.date.till %>%
+#   date.from %>%
+#   cbr.ticker %>%
+#   url %>%
+#   download.ts %>%
+#   write.ts()
+#
+#
+#
+#

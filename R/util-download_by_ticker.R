fd6cf17b6e99ce19ae3d89a744b21d129267f07a
Mikhail Gareev <grvmkhl@gmail.com>
2021-06-06 09:19:14 +0300

    add download_by_ticker methods

---
+++ R/util-download_by_ticker.R
@@ -0,0 +1,86 @@
+download.by.ticker <-  function(object, .ticker) {
+  UseMethod("download.by.ticker")
+}
+setMethod(
+  "download.by.ticker", "cbr",
+  function(object, .ticker) {
+    object <-  object%>%
+    ticker(.ticker) %>%
+        freq() %>%
+        observation.start() %>%
+        use.archive() %>%
+        previous.date.till() %>%
+        date.from() %>%
+        cbr.ticker() %>%
+        url() %>%
+        download.ts() %>%
+        write.ts()
+    validObject(object)
+    return(object)
+  }
+)
+
+setMethod(
+  "download.by.ticker", "moex",
+  function(object, .ticker) {
+    object <-  object%>%
+        ticker(.ticker) %>%
+        observation.start %>%
+        use.archive %>%
+
+        previous.date.till %>%
+        date.from %>%
+
+        download.ts %>%
+        write.ts
+    validObject(object)
+    return(object)
+  }
+)
+
+setMethod(
+  "download.by.ticker", "fred",
+  function(object, .ticker) {
+    object <-  object%>%
+      ticker(.ticker) %>%
+        freq %>%
+        observation.start %>%
+        use.archive %>%
+        previous.date.till %>%
+        date.from %>%
+        download.ts%>%
+        write.ts
+    validObject(object)
+    return(object)
+  }
+)
+
+setMethod(
+  "download.by.ticker", "oecd",
+  function(object, .ticker) {
+    object <-  object%>%
+      ticker(.ticker) %>%
+      observation.start %>%
+        use.archive %>%
+        oecd.ticker %>%
+        url %>%
+        download.ts%>%
+        write.ts
+    validObject(object)
+    return(object)
+  }
+)
+setMethod(
+  "download.by.ticker", "dallasfed",
+  function(object, .ticker) {
+    object <-  object%>%
+      ticker(.ticker) %>%
+        observation.start %>%
+        use.archive %>%
+        url %>%
+        download.ts%>%
+        write.ts
+    validObject(object)
+    return(object)
+  }
+)

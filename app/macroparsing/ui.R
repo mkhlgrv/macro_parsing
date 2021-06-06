431fae42b5ffdddaf3317bd52f5c333941b4a7e5
Mikhail Gareev <grvmkhl@gmail.com>
2021-06-06 18:49:36 +0300

    Remove default comments

--- app/macroparsing/ui.R
+++ app/macroparsing/ui.R
@@ -1,31 +1,19 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(macroparsing)
 library(ggplot2)
 library(dplyr)
 
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
-    # Application title
     titlePanel("База данных для экономики России"),
 
-    # Sidebar with a slider input for number of bins
     fluidRow(
         column(4,
                selectizeInput(
                    "ticker",
                    "Переменные:",
-                   choices =#c('дол' = 'usd'),
+                   choices =
                        {
                            x <- macroparsing::variables$ticker
                            names(x) <- macroparsing::variables$name_rus_short
@@ -42,7 +30,6 @@ shinyUI(fluidPage(
                                   as.character())
         ),
 
-        # Show a plot of the generated distribution
         mainPanel(
             plotOutput("plot")
         )

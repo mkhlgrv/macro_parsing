c600eba19e930547d4366a6ce450546856eb4863
Mikhail Gareev <grvmkhl@gmail.com>
2021-06-08 14:08:43 +0300

    Add means to plot

--- app/macroparsing/server.R
+++ app/macroparsing/server.R
@@ -8,7 +8,13 @@ shinyServer(function(input, output) {
       data.table::fread(paste0(Sys.getenv('directory'),"/data/raw/",
                                ticker, '.csv'),
                         select = c('date', 'value')) %>%
-        dplyr::mutate(ticker = ticker)
+        dplyr::mutate(ticker = ticker) %>%
+        na.omit %>%
+        dplyr::group_by(zoo::as.yearmon(date)) %>%
+        dplyr::mutate(cm_value = cummean(value)) %>%
+        dplyr::mutate(mean_value = mean(value)) %>%
+        dplyr::ungroup() %>%
+        dplyr::mutate(rm_value = zoo::rollmean(value, k = 23, fill = NA, align = "right"))
     }) %>%
       inner_join(macroparsing::variables[,
                                          c('ticker', 'name_rus_short')],
@@ -22,8 +28,10 @@ shinyServer(function(input, output) {
         get.data.from.csv(input$ticker) %>%
           filter(date >= input$daterange[1],
                  date <= input$daterange[2],) %>%
-          ggplot(aes(x=date, y =value))+
-          geom_line()+
+          ggplot(aes(x=date, y =mean_value, color ='mean'))+
+          geom_point()+
+          geom_line(aes(y=cm_value, color='cummean'))+
+          geom_line(aes(y=rm_value, color='rollmean'))+
           theme_minimal()+
           labs(x='Дата', y ='Значение')+
           facet_wrap(vars(name_rus_short), scales = 'free_y')

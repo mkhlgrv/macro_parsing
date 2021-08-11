a0fbb6c9f21172092d882258cbafc4a1376fd050
Mikhail Gareev <43676999+mkhlgrv@users.noreply.github.com>
2021-08-11 16:21:11 +0300

    remove \test

--- app/test/ui.R
+++
@@ -1,5 +0,0 @@
-shinyUI(fluidPage(
-  actionBttn("reset", "RESET", style="simple", size="sm", color = "warning"),
-  actionBttn("add", "ADD", style="simple", size="sm", color = "warning"),
-  verbatimTextOutput(outputId = "text")
-))

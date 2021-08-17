cab0da7e74142a53757a01afe8745772a8c00400
Mikhail Gareev <43676999+mkhlgrv@users.noreply.github.com>
2021-08-17 21:27:06 +0300

    Update rosstat_parsing.R
    readxl instead of xlsx

--- R/rosstat_parsing.R
+++ R/rosstat_parsing.R
@@ -95,13 +95,13 @@ setMethod("download.ts","rosstat",
                                           "q" = "I",
                                           "m" = "Jan",
                                           "m_cumul" = "Year",
-                                          "m_numeric" = "X1",
+                                          "m_numeric" = "^1$",
                                           "q_horizontal" = "I квартал")
               last_period_name <- switch(object@sheet_info$freq,
                                           "q" = "IV",
                                           "m" = "Dec",
                                           "m_cumul" = "Nov",
-                                         "m_numeric" = "X12",
+                                         "m_numeric" = "^12$",
                                          "q_horizontal" = NA)
 
               freq_by <- switch(object@sheet_info$freq,
@@ -125,27 +125,49 @@ setMethod("download.ts","rosstat",
               sheet <- grep(paste0("(^",object@sheet_info$sheet,")( |\\.$|\\. |$)"),
                             readxl::excel_sheets(temp_file))
 
-              res <- readxl::read_xlsx(path = temp_file,
-                                sheet = sheet,
-                                skip =  object@sheet_info$start_row-1,
-                                col_types = freq_cols)
+
+              if(object@sheet_info$freq != "q_horizontal"){
+                res <- readxl::read_excel(path = temp_file,
+                                          sheet = sheet,
+                                          skip =  object@sheet_info$start_row-1)
+              } else{
+                res <- readxl::read_excel(path = temp_file,
+                                          sheet = sheet,
+                                          skip =  object@sheet_info$start_row-1)
+              }
+
+
 
 
               start_row <- grep(pattern = object@sheet_info$header_pattern,
-                                x = res[,object@sheet_info$header_column])[object@sheet_info$n_match]+
+                                x = res[,object@sheet_info$header_column]%>%
+                                  dplyr::pull(1)
+                                )[object@sheet_info$n_match]+
                 object@sheet_info$skip_after_header
 
               if(object@sheet_info$freq != "q_horizontal"){
                 res <-  res[start_row:nrow(res),]
+
                 if(object@sheet_info$end_row_indicator == 'empty_row'){
-                  end_row <- (which(grepl("\\d{4}",res[,object@sheet_info$header_column])==FALSE)-1)[1]
+                  non_year_rows <- which(grepl("\\d{4}",res[,object@sheet_info$header_column] %>%
+                           dplyr::pull(1))==FALSE)
+                  if(length(non_year_rows)==0){
+                    end_row <- nrow(res)
+                  } else{
+                    end_row <- non_year_rows[1]-1
+                  }
+
+
+
                 } else if(object@sheet_info$end_row_indicator == 'next_serie'){
                   end_row <- grep(object@sheet_info$header_pattern,
-                                  res[,object@sheet_info$header_column])[2] - 1
+                                  res[,object@sheet_info$header_column]%>%
+                                    dplyr::pull(1))[2] - 1
                 }
 
 
                 start_year <- res[1,object@sheet_info$header_column] %>%
+                  dplyr::pull(1) %>%
                   substr(start = 1,stop = 4) %>%
                   as.numeric()
 
@@ -159,21 +181,21 @@ setMethod("download.ts","rosstat",
                   columns <- start_column:end_column
                 }
 
-
-
                 # нужно проверять на соответствие типу numeric
+
                 value <-  res[1:end_row,
                               columns] %>%
+                  as.data.frame() %>%
                   t %>%
                   as.matrix %>%
                   as.numeric()
               } else{
 
 
-                year_colnames <- readxl::read_xlsx(path = temp_file,
+
+                year_colnames <- readxl::read_excel(path = temp_file,
                                   sheet = sheet,
-                                  skip =  object@sheet_info$start_row-2,
-                                  col_types = rep("guess", 5))%>%
+                                  skip =  object@sheet_info$start_row-2, )%>%
                   colnames()
 
                 start_year <- stringr::str_match(year_colnames, '\\d{4}') %>%
@@ -182,7 +204,9 @@ setMethod("download.ts","rosstat",
                   as.numeric()
 
 
-                res <-  res[start_row,(object@sheet_info$header_column+1):ncol(res)]
+                res <-  res[start_row,
+                            (object@sheet_info$header_column+1):ncol(res)] %>%
+                  as.data.frame()
                 value <- res %>%
                   as.numeric()
               }

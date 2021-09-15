aa1b40031758bd90dda37db3c417fb6b5654936d
Mikhail Gareev <43676999+mkhlgrv@users.noreply.github.com>
2021-09-15 07:52:16 +0300

    Update internal_calc.R
    Добавил метода расчета construction_real

--- R/internal_calc.R
+++ R/internal_calc.R
@@ -16,8 +16,26 @@ setMethod(
 setMethod(
   "download.ts", "internal",
   function(object) {
-    file_name <- paste0(Sys.getenv('directory'), "/data/raw/", object@related_ticker, ".csv")
-    object@ts <- data.table::fread(file_name, colClasses = c("Date", "numeric", "Date"))
+    if(object@ticker=='construction_real'){
+      file_name_numenator <- paste0(Sys.getenv('directory'), "/data/raw/", 'construction_nom', ".csv")
+      file_name_denominator <- paste0(Sys.getenv('directory'), "/data/raw/", 'ppi_construction', ".csv")
+      numenator <- data.table::fread(file_name_numenator, colClasses = c("Date", "numeric", "Date")) %>%
+        dplyr::group_by(date) %>%
+        dplyr::filter(update_date == max(update_date)) %>%
+        dplyr::ungroup()
+      denominator <- data.table::fread(file_name_denominator, colClasses = c("Date", "numeric", "Date"))%>%
+        dplyr::group_by(date) %>%
+        dplyr::filter(update_date == max(update_date)) %>%
+        dplyr::ungroup()
+      object@ts <- numenator
+      object@ts$value <- (numenator$value/cumprod(denominator$value/100))/
+        (numenator$value/cumprod(denominator$value/100))[1]*100
+
+    } else{
+      file_name <- paste0(Sys.getenv('directory'), "/data/raw/", object@related_ticker, ".csv")
+      object@ts <- data.table::fread(file_name, colClasses = c("Date", "numeric", "Date"))
+    }
+
 
     validObject(object)
     return(object)

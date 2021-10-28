208cf9aafb27997a43bf345b10f614f511227d80
Mikhail Gareev <43676999+mkhlgrv@users.noreply.github.com>
2021-10-28 23:51:17 +0300

    Add montly internal calculations

--- R/internal_calc.R
+++ R/internal_calc.R
@@ -31,9 +31,34 @@ setMethod(
       object@ts$value <- (numenator$value/cumprod(denominator$value/100))/
         (numenator$value/cumprod(denominator$value/100))[1]*100
 
-    } else{
+    } else {
       file_name <- paste0(Sys.getenv('directory'), "/data/raw/", object@related_ticker, ".csv")
-      object@ts <- data.table::fread(file_name, colClasses = c("Date", "numeric", "Date"))
+      ts <- data.table::fread(file_name, colClasses = c("Date", "numeric", "Date"))
+      if(grepl("_montly",object@ticker)){
+        agg_fun <- function(x, ticker){
+          if(ticker=='fer_montly'){
+            x
+          } else{
+            dplyr::mutate(x, value = mean(value,na.rm=TRUE))
+          }
+        }
+        ts <- ts %>%
+          dplyr::group_by(date) %>%
+          dplyr::filter(update_date == max(update_date)) %>%
+          dplyr::ungroup() %>%
+          dplyr::arrange(dplyr::desc(date)) %>%
+          dplyr::group_by(zoo::as.yearmon(date)) %>%
+          agg_fun(ticker = object@ticker) %>%
+          dplyr::filter(dplyr::row_number()==1) %>%
+          dplyr::ungroup() %>%
+          dplyr::select(1:3) %>%
+          dplyr::arrange(date) %>%
+          dplyr::mutate(date = zoo::as.Date(zoo::as.yearmon(date))) %>%
+          dplyr::filter(date < max(date))
+
+
+      }
+      object@ts <- ts
     }
 
 
